84f3c204c3750622785fc66c850f37d47b3bcfbc
dan0102dan <me@dan0102dan.ru>
2023-10-03 00:18:10 +0300

    меня отругали за стили

--- web/src/routes/Swap/Swap.jsx
+++ web/src/routes/Swap/Swap.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { server } from '../../API'
-import styles from './Swap.module.css'
 
 const Swap = () => {
     let { instId } = useParams()
