743ccaedaeb9fc3b46b154964341fa176fec4c2f
dan0102dan <me@dan0102dan.ru>
2023-10-03 22:59:30 +0300

    может, из-за палочки ошибка

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
   },
   {
-    path: 'ccy/:instId',
+    path: '/ccy/:instId',
     element: <Swap />
   }
 ], { basename: '/exchangel' })
