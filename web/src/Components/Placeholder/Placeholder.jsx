445b4d3f9558ad157ed32f5ffe7224aff607b3f8
dan0102dan <me@dan0102dan.ru>
2023-10-04 22:49:40 +0300

    теперь нет дерганий на главном экране

--- web/src/Components/Placeholder/Placeholder.jsx
+++ web/src/Components/Placeholder/Placeholder.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import styles from './Placeholder.module.css'
+import { Section } from '../index'
 
 const Placeholder = ({ icon, title, description, action, errorInfo }) => {
     return (
-        <div className={styles.placeholder}>
-            <div className={styles.icon}>{icon}</div>
-            <div className={styles.title}>{title}</div>
-            <div className={styles.description}>{description}</div>
-            {errorInfo && <div className={styles.errorInfo}>{errorInfo}</div>}
-            {action}
-        </div>
+        <Section>
+            <div className={styles.placeholder}>
+                <div className={styles.icon}>{icon}</div>
+                <div className={styles.title}>{title}</div>
+                <div className={styles.description}>{description}</div>
+                {errorInfo && <div className={styles.errorInfo}>{errorInfo}</div>}
+                {action}
+            </div>
+        </Section>
     )
 }
 
